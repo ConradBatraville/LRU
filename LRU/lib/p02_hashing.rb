639f01b06975ef51cbdb3374f37058729bf4b11b
Conrad Batraville <theradistone@gmail.com>
2018-12-07 12:10:59 -0500

    Hashing Complete

--- LRU/lib/p02_hashing.rb
+++ LRU/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |el, idx|
+      sum += (idx * el)
+    end
+    sum.hash
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.chars.each_with_index do |el, idx|
+      sum += (idx * el.ord)
+    end
+    sum.hash
   end
 end
 
@@ -16,6 +26,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.to_a.sort.each do |sub_arr|
+      sub_arr.each_with_index do |el, idx|
+        sum += (idx * el.to_s.ord)
+      end
+    end
+    sum
   end
 end
